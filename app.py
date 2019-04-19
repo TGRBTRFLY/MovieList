a5a1c6459ec649e01ac36710c31d8c9f07735f48
PagePetty <pagevpetty@live.com>
2019-04-18 23:18:41 -0600

    Initial commit - add movies

---
+++ app.py
@@ -0,0 +1,64 @@
+"""
+Enter
+'a' to add a movie,
+'l' to see your movies,
+'f' to find a movie, and
+'q' to quit:
+
+
+Tasks:
+[x]: Decide where to store movies
+[]: What is the format of a movie?
+[x]: Show the user the main interface and get their input
+[]: Allow users to add movies
+[]: Show all their movies
+[]: Find a movie
+[x]: Stop running the program when they type 'q'
+"""
+
+movies = []
+
+"""
+movie - {
+    'name' 
+"""
+
+
+def menu():
+    user_input = input("Enter \n"
+                       "'a' to add a movie\n"
+                       "'l' to see your movies\n"
+                       "'f' to find a movie\n"
+                       "'q' to quit:\n")
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_moies()
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print('Unknown command, please try again.')
+
+        user_input = input("Enter \n"
+                           "'a' to add a movie\n"
+                           "'l' to see your movies\n"
+                           "'f' to find a movie, and\n"
+                           "'q' to quit:\n")
+
+
+def add_movie():
+    name = input('Enter the movie name: ')
+    director = input('Enter the movie director: ')
+    year = int(input('Enter the movie release year: '))
+
+    movies.append({
+        'name': name,
+        'director': director,
+        'year': year
+    })
+
+
+menu()
+
+print(movies)
